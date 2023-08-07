678e15668d2c8e8931bc6c84316981d2ff2cbbd2
alfonsoristorato <alfonsoristorato@gmail.com>
2023-08-07 09:14:22 +0100

    changes to getTokens to allow getting of multiple tokens

--- src/main/java/com/alfonsoristorato/igdbscraper/service/twitch/TwitchAuthenticatorService.java
+++ src/main/java/com/alfonsoristorato/igdbscraper/service/twitch/TwitchAuthenticatorService.java
@@ -1,9 +1,11 @@
 package com.alfonsoristorato.igdbscraper.service.twitch;
 
 import com.alfonsoristorato.igdbscraper.models.IGDBHeaders;
+import com.alfonsoristorato.igdbscraper.models.TwitchResponse;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -15,22 +17,22 @@ public class TwitchAuthenticatorService {
     private final String CLIENT_ID;
     private final List<String> CLIENT_SECRETS;
     private final String GRANT_TYPE;
+
     public TwitchAuthenticatorService(TwitchAuthenticator twitchAuthenticator, TwitchConfigProperties twitchConfigProperties) {
         this.twitchAuthenticator = twitchAuthenticator;
         this.CLIENT_ID = twitchConfigProperties.clientId();
         this.CLIENT_SECRETS = twitchConfigProperties.clientSecrets();
         this.GRANT_TYPE = twitchConfigProperties.grantType();
     }
+    //TODO: explore if the below can return a flux of headers
+    public Mono<List<IGDBHeaders>> getTokens() {
+        return Flux.fromIterable(CLIENT_SECRETS)
+                .flatMap(secret -> twitchAuthenticator.authenticate(CLIENT_ID, secret, GRANT_TYPE))
+                .map(this::transformTwitchResponse)
+                .collectList();
+    }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public Mono<IGDBHeaders> getToken() {
-        return twitchAuthenticator.authenticate(CLIENT_ID, CLIENT_SECRETS.get(0), GRANT_TYPE)
-                .map(response ->
-
-                        new IGDBHeaders(CLIENT_ID, String.join(" ", response.tokenType(), response.accessToken()))
-
-                );
-
-
+    private IGDBHeaders transformTwitchResponse(TwitchResponse twitchResponse) {
+        return new IGDBHeaders(CLIENT_ID, String.join(" ", twitchResponse.tokenType(), twitchResponse.accessToken()));
     }
 }

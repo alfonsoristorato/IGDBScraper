e78e14d5395a37974b5c4b07b7eba7f96d7e0a7f
alfonsoristorato <alfonsoristorato@gmail.com>
2023-08-26 10:03:08 +0100

    implemented a 4 per second requests to the API, only 1 token

--- src/main/java/com/alfonsoristorato/igdbscraper/service/igdb/IgdbService.java
+++ src/main/java/com/alfonsoristorato/igdbscraper/service/igdb/IgdbService.java
@@ -6,6 +6,9 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class IgdbService {
     private final IgdbClient igdbClient;
@@ -19,13 +22,22 @@ public class IgdbService {
     // repeat operation below until full db has been scraped changing the offset based on the last ID received
     // limitations: 4 requests per sec, 500 records per request
     @EventListener(ApplicationReadyEvent.class)
-    public void getDataFromIgdb() {
-        Flux.fromIterable(twitchAuthenticatorService.getTokens()
-                        .flatMapIterable(el -> el).toIterable()
-                )
+    public void getDataFromIgdb() throws InterruptedException {
+        AtomicLong offset = new AtomicLong(0);
+
+        Flux.interval(Duration.ofMillis(250))
+                .flatMap(time -> twitchAuthenticatorService.getTokens())
+                .flatMapIterable(igdbHeadersList -> igdbHeadersList)
                 .flatMap(igdbHeaders ->
-                        igdbClient.requestGameData(igdbHeaders.clientId(), igdbHeaders.authorization(), "fields name; limit 500; sort id asc; offset 501;"))
-                .doOnNext(System.out::println).subscribe();
+                        igdbClient.requestGameData(
+                                igdbHeaders.clientId(),
+                                igdbHeaders.authorization(),
+                                String.format("fields name; limit 500; sort id asc; offset %s;", offset.getAndAdd(500))))
+                .doOnError(System.out::println)
+                .onTerminateDetach()
+//                .doOnNext(System.out::println)
+                .subscribe();
+
 
     }
 
